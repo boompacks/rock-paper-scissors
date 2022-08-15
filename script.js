aa083fd3ce2249d6c465902eaa883fb131475b79
Gabriele Agosta <gabriele@Omen15-Gabriele.localdomain>
2022-08-15 18:01:07 +0200

    Added functionality to play 5 games in a row

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-function getComputerChoice(){
+function getComputerSelection(){
     let choises = ["rock", "paper", "scissors"];
     let randomNumber = Math.random()* 2;
     return choises[parseInt(randomNumber)]
 }
 
-/*
-function playerSelection(){
+
+function getPlayerSelection(){
     let playerChoice = prompt("Make your choice:");
     return playerChoice.toLowerCase()
 }
-*/
+
 
 function playRound(playerSelection, computerSelection){
-    if (computerSelection == "rock"){
-        return "Tie"
-    }else if (computerSelection == "paper"){
-        return `You lose! ${computerSelection} beats ${playerSelection}`
-    }else{
-        return `You win! ${playerSelection} beats ${computerSelection}`
+    switch (playerSelection){
+        case "paper":
+            if (computerSelection == "paper"){
+                return "Tie"
+            }else if (computerSelection == "scissors"){
+                return `You lose! ${computerSelection} beats ${playerSelection}`
+            }else{
+                return `You win! ${playerSelection} beats ${computerSelection}`
+            }
+        case "rock":
+            if (computerSelection == "rock"){
+                return "Tie"
+            }else if (computerSelection == "paper"){
+                return `You lose! ${computerSelection} beats ${playerSelection}`
+            }else{
+                return `You win! ${playerSelection} beats ${computerSelection}`
+            }
+        case "scissors":
+            if (computerSelection == "rock"){
+                return `You lose! ${computerSelection} beats ${playerSelection}`
+            }else if (computerSelection == "paper"){
+                return `You win! ${playerSelection} beats ${computerSelection}`
+            }else{
+                return `Tie`
+            }
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-const result = playRound(playerSelection, computerSelection);
+
+function game(){
+    for (let i=0; i<5; i++){
+        let result = playRound(getPlayerSelection(), getComputerSelection());
+        console.log(result)
+    }
+}
+
+game();
