f5498b4fcbb92391f7449f9907ff21a07dce8bb9
Gabriele Agosta <gabriele@Omen15-Gabriele.localdomain>
2022-08-15 17:46:04 +0200

    Added functionality to let the player make his choice

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function getComputerChoice(){
-    let choises = ["Rock", "Paper", "Scissors"];
+    let choises = ["rock", "paper", "scissors"];
     let randomNumber = Math.random()* 2;
     return choises[parseInt(randomNumber)]
 }
+
+
+function playerSelection(){
+    let playerChoice = prompt("Make your choice:");
+    return playerChoice.toLowerCase()
+}
+
+
+function playRound(){
+    let computerChoice = getComputerChoice();
+    let playerChoice = playerSelection();
+}
+
+
+playRound();
