021f8979a3d3a460f8068053144db582cdb0fcd9
Gabriele Agosta <gabriele@Omen15-Gabriele.localdomain>
2022-08-21 19:17:46 +0200

    Added css to make the game more intuitive

--- script.js
+++ script.js
@@ -10,34 +10,43 @@ function playRound(playerSelection, computerSelection){
         case "paper":
             if (computerSelection == "paper"){
                 displayResult.textContent = "Tie";
+                displayResult.setAttribute('style', 'color:black');
                 return "tie"
             }else if (computerSelection == "scissors"){
                 displayResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+                displayResult.setAttribute('style', 'color:red');
                 return 'lose'
             }else{
                 displayResult.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+                displayResult.setAttribute('style', 'color:green');
                 return "win"
             }
         case "rock":
             if (computerSelection == "rock"){
                 displayResult.textContent = "Tie";
+                displayResult.setAttribute('style', 'color:black');
                 return "tie"
             }else if (computerSelection == "paper"){
                 displayResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+                displayResult.setAttribute('style', 'color:red');
                 return "lose"
             }else{
                 displayResult.textContent = `You win! ${playerSelection} beats ${computerSelection}`
+                displayResult.setAttribute('style', 'color:green');
                 return "win";
             }
         case "scissors":
             if (computerSelection == "rock"){
                 displayResult.textContent =`You lose! ${computerSelection} beats ${playerSelection}`;
+                displayResult.setAttribute('style', 'color:red');
                 return "lose"
             }else if (computerSelection == "paper"){
                 displayResult.textContent =`You win! ${playerSelection} beats ${computerSelection}`;
+                displayResult.setAttribute('style', 'color:green');
                 return "win"
             }else{
                 displayResult.textContent = "Tie";
+                displayResult.setAttribute('style', 'color:black');
                 return `tie`
             }
     }
@@ -49,9 +58,10 @@ function startGame(){
     if (result === 'win') playerScore++;
     if (result === 'lose') computerScore++;
 
-    displayScore.textContent = `Player: ${playerScore} \nComputer: ${computerScore} `;
+    displayPlayerScore.textContent = `Player: ${playerScore}`;
+    displayComputerScore.textContent = `Computer: ${computerScore} `;
     if (playerScore === 5 || computerScore === 5){
-        result = "The game is over!"
+        result = "The game is over! Press one of the buttons to start a new game."
         displayResult.textContent = result;
         playerScore = 0;
         computerScore = 0;
@@ -62,7 +72,8 @@ function startGame(){
 const buttons = document.querySelectorAll("button");
 const container = document.querySelector("#container");
 const displayResult = document.createElement("div");
-const displayScore = document.createElement("div");
+const displayPlayerScore = document.createElement("div");
+const displayComputerScore = document.createElement("div");
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
@@ -70,11 +81,15 @@ let playerSelection = '';
 let playerScore = 0;
 let computerScore = 0;
 
+displayResult.textContent = "Press one of the buttons above to start the game.";
+displayResult.setAttribute('id', 'borders')
+
 container.appendChild(displayResult);
-container.appendChild(displayScore);
+container.appendChild(displayPlayerScore);
+container.appendChild(displayComputerScore);
 
-displayScore.textContent = `Player: ${playerScore} \nComputer: ${computerScore} `;
-
+displayPlayerScore.textContent = `Player: ${playerScore}`;
+displayComputerScore.textContent = `Computer: ${computerScore} `;
 
 rock.addEventListener('click', () => {
     playerSelection = 'rock';
